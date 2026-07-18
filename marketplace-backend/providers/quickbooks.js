58c28a8048ff7a344fd7f9a9942054138402bd6f
Jason Massie <massiejay@gmail.com>
2026-07-17 23:29:38 -0400

    quickbooks

---
+++ marketplace-backend/providers/quickbooks.js
@@ -0,0 +1,6 @@
+// Example structure for quickbooks.js
+export const qboConfigured = () => !!(process.env.QBO_CLIENT_ID && process.env.QBO_CLIENT_SECRET);
+export const qboAuthorizeUrl = (state) => { /* logic to build URL */ };
+export const qboExchangeCode = async (code) => { /* logic to exchange code */ };
+export const qboEnsureToken = async (creds) => { /* logic to refresh */ };
+export const qboCompanyName = async (creds) => { /* logic to fetch name */ };
