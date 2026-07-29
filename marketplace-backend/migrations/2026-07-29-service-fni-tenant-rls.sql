f757c39ea7105f6990088417ee0fb12478ec4b31
jaysus7 <massiejay@gmail.com>
2026-07-29 14:27:37 -0400

    Add tenant RLS for service and F&I data

---
+++ marketplace-backend/migrations/2026-07-29-service-fni-tenant-rls.sql
@@ -0,0 +1,7 @@
+DO $$
+DECLARE tbl text;
+BEGIN
+  FOREACH tbl IN ARRAY ARRAY['credit_applications','fni_products','lenders','parts','part_txns','repair_orders','ro_lines','recon','reconciliations','vehicle_history_reports','trade_appraisals'] LOOP
+    EXECUTE format('CREATE POLICY "Tenant isolation" ON public.%I FOR ALL TO authenticated USING (dealership_id = (SELECT private.current_dealership_id())) WITH CHECK (dealership_id = (SELECT private.current_dealership_id()))', tbl);
+  END LOOP;
+END $$;
