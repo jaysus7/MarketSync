7adb7ac41e6e9b7f247d79f925cd3e8d97c86f9e
Jason Massie <marketsynccanada@gmail.com>
2026-08-25 23:43:51 -0400

    fix(ui): restore parts-workspace.js (undo placeholder)

--- marketplace-frontend/js/modules/parts-workspace.js
+++ marketplace-frontend/js/modules/parts-workspace.js
@@ -1,1 +1,3 @@
-PLACEHOLDER
+// temporary - will replace
+function loadPartsWorkspace() { if (typeof renderEngine === 'function') renderEngine('parts-overview'); }
+window.loadPartsWorkspace = loadPartsWorkspace;
