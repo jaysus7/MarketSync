ef38d264cba1f21f0736bc08b15f88edd78adf66
Jason Massie <marketsynccanada@gmail.com>
2026-09-03 23:27:20 -0400

    chore(staging): cache-bust context toolbar animations

--- marketplace-frontend/js/modules/studio/document-model.js
+++ marketplace-frontend/js/modules/studio/document-model.js
@@ -81,7 +81,7 @@
     '/js/modules/studio/studio-elements-upgrade.js?v=20260904_elements_v1',
     '/js/modules/studio/studio-unify-elements.js?v=20260904_unify_v1',
     '/js/modules/studio/studio-template-previews.js?v=20260904_tmpl_preview_v1',
-    '/js/modules/studio/studio-context-toolbar.js?v=20260904_ctx_toolbar_v1'
+    '/js/modules/studio/studio-context-toolbar.js?v=20260904_ctx_toolbar_v2'
   ].forEach(function (src) {
     var s = document.createElement('script');
     s.src = src;
